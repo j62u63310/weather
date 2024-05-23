7fb69f010ccf22ad11983735f20ae7623393af59
sa8a5 <sa8a56@gmail.com>
2024-05-23 15:33:01 +0800

    天氣API連接

--- src/main/java/com/j62u63310/server/controller/WeatherController.java
+++ src/main/java/com/j62u63310/server/controller/WeatherController.java
@@ -1,27 +1,26 @@
 package com.j62u63310.server.controller;
 
 import com.j62u63310.server.model.Weather;
-import com.j62u63310.server.service.weatherService;
+import com.j62u63310.server.service.WeatherService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.lang.NonNull;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Arrays;
-
 @Validated
 @RestController
 @CrossOrigin(origins = "http://localhost:3000", allowCredentials = "true")
-public class weatherController {
+public class WeatherController {
 
     @Autowired
-    private weatherService testService;
+    private WeatherService testService;
     @GetMapping("/api/weather")
-    public ResponseEntity<Weather> getWeather(@RequestParam String city) {
+    public ResponseEntity<Weather> getWeather(@NonNull @RequestParam String city) {
         System.out.println(city);
         Weather weather = testService.getWeather(city);
         return ResponseEntity.status(HttpStatus.OK).body(weather);
--- src/main/java/com/j62u63310/server/service/WeatherService.java
+++ src/main/java/com/j62u63310/server/service/WeatherService.java
@@ -1,6 +1,7 @@
 package com.j62u63310.server.service;
 
 import com.j62u63310.server.model.Weather;
+import lombok.NonNull;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
@@ -9,13 +10,13 @@ import org.springframework.web.util.UriComponents;
 import org.springframework.web.util.UriComponentsBuilder;
 
 @Service
-public class weatherService {
+public class WeatherService {
 
     private final RestTemplate restTemplate = new RestTemplate();
     private final String apiKey = "CWA-621F40F0-AF1A-4F52-B36B-91EE222EB0EC";
     private final String apiUrl = "https://opendata.cwa.gov.tw/api/v1/rest/datastore/F-C0032-001";
 
-    public Weather getWeather(String city) {
+    public Weather getWeather(@NonNull String city) {
         UriComponents uriBuilder = UriComponentsBuilder.fromHttpUrl(apiUrl)
                 .queryParam("Authorization", apiKey)
                 .queryParam("locationName", city)
